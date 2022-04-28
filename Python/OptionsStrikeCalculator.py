8a54e6ec549aa43cb42c03994a9d610f04f785b3
TMc42 <tmclean1212@gmail.com>
2022-04-28 18:03:03 -0500

    Started kivy class

--- Python/OptionsStrikeCalculator.py
+++ Python/OptionsStrikeCalculator.py
@@ -5,4 +5,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
-class 
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
